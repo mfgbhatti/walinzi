199d1d1925fb9b3431b146ffc5c2ecbaa11030a1
farhan ghani <13787498+mfgbhatti@users.noreply.github.com>
2022-11-16 16:45:33 +0000

    working backend

--- backend/clients/urls.py
+++ backend/clients/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views
+from .views import ClientViewset
+
 
 urlpatterns = [
-  path('all', views.all),
-  path('create', views.create),
-  path('search/<uuid:pk>', views.search),
-  path('update/<uuid:pk>', views.update),
-  path('delete/<uuid:pk>', views.delete)
+  path('all', ClientViewset.as_view({'get': 'list'})),
+  path('create', ClientViewset.as_view({'post': 'create'})),
+  path('search/<uuid:pk>', ClientViewset.as_view({'get': 'retrieve'})),
+  path('update/<uuid:pk>', ClientViewset.as_view({'put': 'update'})),
+  path('delete/<uuid:pk>', ClientViewset.as_view({'delete': 'destroy'}))
 ]
--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,60 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from .models import Client
 from .serializers import ClientSerializer
 
 
-@api_view(["GET"])
-def all(request):
-    result = ClientSerializer(Client.objects.all(), many=True).data
-    return JsonResponse(result, safe=False, status=status.HTTP_200_OK)
-
-
-@api_view(["GET"])
-def search(request, pk):
-    try:
-        result = Client.objects.get(pk=pk)
-    except Client.DoesNotExist:
-        return JsonResponse(status=status.HTTP_404_NOT_FOUND)
-
-    serilized_data = ClientSerializer(result, many=False).data
-    return JsonResponse(serilized_data, safe=False, status=status.HTTP_200_OK)
-
-
-@api_view(["POST"])
-def create(request):
-    data = JSONParser().parse(request)
-    serializer = ClientSerializer(data=data).data
-    if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(serializer, status=status.HTTP_201_CREATED)
-    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["PUT"])
-def update(request, pk):
-    try:
-        result = Client.objects.get(pk=pk)
-    except Client.DoesNotExist:
-        return JsonResponse(status=status.HTTP_404_NOT_FOUND)
-
-    data = JSONParser().parse(request)
-    serializer = ClientSerializer(result, data=data).data
-    if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(serializer, safe=False, status=status.HTTP_202_ACCEPTED)
-    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["DELETE"])
-def delete(request, pk):
-    try:
-        result = Client.objects.get(pk=pk)
-    except Client.DoesNotExist:
-        return JsonResponse(status=status.HTTP_404_NOT_FOUND)
-
-    result.delete()
-    return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+class ClientViewset(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
