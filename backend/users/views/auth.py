565e77361dc43e89ffb90c6399eb4435d5ee838c
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2022-12-19 17:06:27 +0000

    added logout

--- backend/users/views/auth.py
+++ backend/users/views/auth.py
@@ -19,6 +19,7 @@ class LogoutView(views.APIView):
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
+            logout(request)
 
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
