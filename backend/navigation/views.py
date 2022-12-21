88fd7d3d5c52dc54ed221e45231467916d3d9761
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2022-12-21 14:18:11 +0000

    added list function on navigation views

--- backend/navigation/views.py
+++ backend/navigation/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .models import MainNavigation
 
 from .serializers import NavigationSerializer
+
 
 class NavigationViewSet(viewsets.ModelViewSet):
     """Navigation viewset."""
@@ -11,3 +13,19 @@ class NavigationViewSet(viewsets.ModelViewSet):
     serializer_class = NavigationSerializer
     permission_classes = (IsAuthenticated,)
     queryset = MainNavigation.objects.all()
+
+    def list(self, request, format=None):
+        """List."""
+        # send all navigation items as defaultNavigation, compactNavigation
+        # and fullNavigation
+        queryset = self.get_queryset()  # get all navigation items
+        serializer = NavigationSerializer(queryset, many=True)
+        response = Response(
+            {
+                "compact": serializer.data,
+                "default": serializer.data,
+                "futuristic": serializer.data,
+                "horizontal": serializer.data,
+            }
+        )
+        return response
