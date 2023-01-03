8f70d30b2feb969589553a34eb652a8e485e404a
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-01-03 15:29:34 +0000

    edited settings

--- src/app/modules/admin/settings/account/account.component.ts
+++ src/app/modules/admin/settings/account/account.component.ts
@@ -1,24 +1,34 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
+import {
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
+import { UserService } from '@core/user/user.service';
+import { User } from '@core/user/user.types';
 
 @Component({
-    selector       : 'settings-account',
-    templateUrl    : './account.component.html',
-    encapsulation  : ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'settings-account',
+    templateUrl: './account.component.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsAccountComponent implements OnInit
-{
+export class SettingsAccountComponent implements OnInit {
     accountForm: UntypedFormGroup;
+    user: User;
 
     /**
      * Constructor
      */
     constructor(
-        private _formBuilder: UntypedFormBuilder
-    )
-    {
-    }
+        private readonly _formBuilder: UntypedFormBuilder,
+        private readonly _userService: UserService
+    ) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Lifecycle hooks
@@ -27,19 +37,24 @@ export class SettingsAccountComponent implements OnInit
     /**
      * On init
      */
-    ngOnInit(): void
-    {
-        // Create the form
+    ngOnInit(): void {
+
+    }
+
+    // Create the form
+    buildForm(): void {
         this.accountForm = this._formBuilder.group({
-            name    : ['Brian Hughes'],
+            name: ['Brian Hughes'],
             username: ['brianh'],
-            title   : ['Senior Frontend Developer'],
-            company : ['YXZ Software'],
-            about   : ['Hey! This is Brian; husband, father and gamer. I\'m mostly passionate about bleeding edge tech and chocolate! 🍫'],
-            email   : ['hughes.brian@mail.com', Validators.email],
-            phone   : ['121-490-33-12'],
-            country : ['usa'],
-            language: ['english']
+            title: ['Senior Frontend Developer'],
+            company: ['YXZ Software'],
+            about: [
+                "Hey! This is Brian; husband, father and gamer. I'm mostly passionate about bleeding edge tech and chocolate! 🍫",
+            ],
+            email: ['hughes.brian@mail.com', Validators.email],
+            phone: ['121-490-33-12'],
+            country: ['usa'],
+            language: ['english'],
         });
     }
 }
