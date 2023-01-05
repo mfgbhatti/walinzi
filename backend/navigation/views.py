7c24d9e6510862e4d8757584dd5b31e25fb02c4c
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-01-05 15:07:55 +0000

    updated navigation to send diffrent data

--- backend/navigation/views.py
+++ backend/navigation/views.py
@@ -1,4 +1,5 @@
 """Navigation views."""
+import copy
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -19,15 +20,23 @@ class NavigationViewSet(viewsets.ModelViewSet):
         user_groups = self.request.user.groups.all()
         return Navigation.objects.filter(visible_to__in=user_groups, parent=None).prefetch_related("children")
 
-    def list(self, request, format=None):
+    def list(self, request):
         """List."""
         queryset = self.get_queryset()  # get all navigation items
         serializer = NavigationSerializer(queryset, many=True)
+        compact = copy.deepcopy(serializer.data)
+        futuristic = copy.deepcopy(serializer.data)
+        for key in compact:
+            key["type"] = "aside"
+            key["tooltip"] = key["title"]
+
+        for key in futuristic:
+            key["title"] = key["title"].upper()
         return Response(
             {
-                "compact": serializer.data,
+                "compact": compact,
                 "default": serializer.data,
-                "futuristic": serializer.data,
+                "futuristic": futuristic,
                 "horizontal": serializer.data,
             }
         )
