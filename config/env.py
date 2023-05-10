2f898099ac70e4feecba1953c421dea6cac52bc1
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-05-10 20:01:58 +0100

    added all settings

---
+++ config/env.py
@@ -0,0 +1,20 @@
+"""
+Environment from django
+"""
+import environ
+from django.core.exceptions import ImproperlyConfigured
+
+env = environ.Env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = environ.Path(__file__) - 2
+
+
+def env_to_enum(enum_cls, value):
+    for x in enum_cls:
+        if x.value == value:
+            return x
+
+    raise ImproperlyConfigured(f"Env value {repr(value)} could not be found in {repr(enum_cls)}")
---
+++ config/settings/production.py
@@ -0,0 +1,22 @@
+"""
+Django settings for production environment.
+"""
+
+from config.env import env
+from config.settings.base import *  # noqa
+
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
+
+SECRET_KEY = env("SECRET_KEY", default="000000000000000000000000000000000000000000000000000000000000000")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+CORS_ALLOWED_ORIGINS = False
+CORS_ORIGIN_WHITELIST = env.list("CORS_ORIGIN_WHITELIST", default=[])
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+APP_DOMAIN = env("APP_DOMAIN", default="http://localhost:8000")
+
+WSGI_APPLICATION = "config.wsgi.application"
