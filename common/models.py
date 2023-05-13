f90f70580afde6c2a46f90bd129337023eedc931
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-05-13 20:00:51 +0100

    added common model layouts

---
+++ common/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    """Base model."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
+        get_latest_by = "created_at"
+        ordering = ["-created_at", "-updated_at"]
+
+
+class BaseAddress(models.Model):
+    """Base address model."""
+
+    street = models.CharField(
+        max_length=255,
+    )
+    city = models.CharField(
+        max_length=30,
+    )
+
+    post_code = models.CharField(
+        max_length=15,
+    )
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
+
+
+class BaseDetail(models.Model):
+    """Base detail model."""
+
+    vat_number = models.CharField(max_length=20, blank=True)
+    website = models.URLField(
+        max_length=100,
+    )
+
+    class Meta:
+        """Meta option."""
+
+        abstract = True
