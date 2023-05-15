96a6be5d6ea3492c39556385650225d42605e04c
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-05-15 21:36:20 +0100

    updated dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,17 @@
 """
 Views for dashboard
 """
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 @login_required
 def Dashboard(request):
     """main view"""
-    template = loader.get_template("dashboard/index.html")
+
     context = {
-        'user.is_authenticated': 'request.user.is_authenticated'
+        "user.is_authenticated": request.user.is_authenticated,
+        "dashboard_active": "active"
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "dashboard/index.html", context)
