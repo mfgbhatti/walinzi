376cc01d54c9769ed11f9b2504dc8be4f05528cd
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-05-30 19:38:22 +0100

    working licence component getting details and saving them

--- staff/views/licence.py
+++ staff/views/licence.py
@@ -7,13 +7,14 @@ from django.contrib.auth.decorators import login_required
 from django.forms.models import model_to_dict
 from datetime import datetime
 
-from staff.models import StaffLicence
+from staff.models import Staff, StaffLicence
 from staff.forms import StaffLicenceForm
 from staff.utils import SiaSearchComponent
 
 
 @login_required
 def StaffLicenceView(request, staff_id):
+    staff = Staff.objects.get(id=staff_id)
     try:
         licence = StaffLicence.objects.get(staff=staff_id)
     except StaffLicence.DoesNotExist:
@@ -25,25 +26,26 @@ def StaffLicenceView(request, staff_id):
             sia_search = SiaSearchComponent()
             licence_no = request.POST.get("licence_no")
             licence = form.save(commit=False)
-            result = sia_search.submit(licence_no=licence_no)
+            result = sia_search.submit(licence_no=licence_no)  # get data from sia on the licence number
             my_list = (
                 "registered",
                 "expiry_date",
             )
-            for value in my_list:
-                date_str = result.get(value)
+            for key in my_list:
+                date_str = result.get(key)
 
                 try:
-                    date_value = datetime.strptime(date_str, "%d %B %Y").date()
+                    date_value = datetime.strptime(date_str, "%d %B %Y").date()  # change date format
                 except ValueError:
                     date_value = None
 
                 if date_value is not None:
-                    result[value] = date_value.strftime("%Y-%m-%d")
+                    result[key] = date_value.strftime("%Y-%m-%d")
 
             licence_data = model_to_dict(licence)
             licence_data.update(result)
             licence = StaffLicence(**licence_data)
+            licence.staff = staff
             licence.save()
             # print(result)
             return redirect("staff:staff_detail", staff_id)
