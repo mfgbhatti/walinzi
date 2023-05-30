38eec1c1844a9904ff3f037ac758b70dd64e3c0e
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-05-30 19:12:14 +0100

    small changes to convey message better

--- sitestatic/js/script.js
+++ sitestatic/js/script.js
@@ -1,7 +1,7 @@
 function copyURI(evt) {
 evt.preventDefault();
 navigator.clipboard.writeText(evt.target.getAttribute('href')).then(() => {
-    evt.target.innerHTML = "Copied link"
+    evt.target.innerHTML = "Copied"
     /* clipboard successfully set */
 }, () => {
     evt.target.innerHTML = "Failed"
