639ffee7f1b67343e8ca4505bb444a4963dd5a1e
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-06-14 00:21:01 +0100

    csrf cockie

--- sitestatic/js/script.js
+++ sitestatic/js/script.js
@@ -11,3 +11,21 @@ function copyURI(evt) {
     }
   );
 }
+
+// get csrf_token
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      // delete white space
+      var cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
