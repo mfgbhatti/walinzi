3b4891558af854ffd75946ebf15527fb985cb25f
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-06-14 17:38:11 +0100

    new model timesheet

--- shifts/admin.py
+++ shifts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from shifts.models import Shift
+from shifts.models import Timesheet
 
 # Register your models here.
 
 admin.site.register(Shift)
+admin.site.register(Timesheet)
--- shifts/models.py
+++ shifts/models.py
@@ -7,7 +7,7 @@ from staff.models import Staff
 
 class Shift(BaseModel):
     site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name="shifts", null=True, blank=True)
-    staff = models.ManyToManyField(Staff)
+    # staff = models.ManyToManyField(Staff) # removed because shift will be linked to staff throught timesheet model
     time_in = models.DateTimeField()
     time_out = models.DateTimeField()
     is_active = models.BooleanField(default=True)
@@ -18,9 +18,9 @@ class Shift(BaseModel):
     def name(self):
         raise AttributeError("'Shift' object has no attribute 'name'")
 
-    @classmethod
-    def get_shifts_for_staff(cls, staff):
-        return cls.objects.filter(staff=staff)
+    # @classmethod
+    # def get_shifts_for_staff(cls, staff):
+    #     return cls.objects.filter(staff=staff)
 
     def duration(self):
         result = self.time_out - self.time_in
@@ -29,11 +29,14 @@ class Shift(BaseModel):
         return "{:.2f}".format(hours, 2)
 
     def __str__(self):
-        guard = ", ".join(str(seg) for seg in self.staff.all())
-        return f"{self.time_in.strftime('%d/%m/%Y')} {self.site} is coverd by {guard}."
+        return f"{self.time_in.strftime('%d/%m/%Y')} for {self.site}."
 
     class Meta:
         ordering = ("time_in", "time_out")
+        db_table = "shifts"
+
+        verbose_name = "Shift"
+        verbose_name_plural = "Shifts"
 
 
 # class ShiftSchedule(models.Model):
@@ -41,3 +44,26 @@ class Shift(BaseModel):
 #     started = models.DateField()
 #     ended = models.DateField()
 #     shifts = models.ManyToManyField(Shift)
+class Timesheet(models.Model):
+    """this will be mainly addressing staff time"""
+
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="timesheet", null=True, blank=True)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE, related_name="timesheet", null=True, blank=True)
+    time_in = models.DateTimeField()
+    time_out = models.DateTimeField()
+
+    def duration(self):
+        result = self.time_out - self.time_in
+        hours = result.total_seconds() / 3600  # Convert duration to hours
+        # return "%.2f" % hours # Format to 2 decimal places
+        return "{:.2f}".format(hours, 2)
+
+    def __str__(self):
+        return f"{self.time_in.strftime('%d/%m/%Y')} for {self.staff}."
+
+    class Meta:
+        ordering = ("time_in", "time_out")
+        db_table = "timesheets"
+
+        verbose_name = "Timesheet"
+        verbose_name_plural = "Timesheets"
