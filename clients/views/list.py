fd37059e16bbb3a22387e13b59059ce041e9044f
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-07-01 09:48:43 +0100

    fixed urls for clients

--- clients/views/list.py
+++ clients/views/list.py
@@ -29,7 +29,7 @@ def ClientList(request):
             client = form.save(commit=False)
             client.customer = user.customer
             client.save()
-            return redirect("clients:client_list")
+            return redirect("clients:index")
 
     context = {
         "user.is_authenticated": request.user.is_authenticated,
