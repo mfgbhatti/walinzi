4b95d9e6f352b95f7a56f00a91a53a28dab302f6
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-07-07 09:51:49 +0100

    option to search menu item

--- sitestatic/js/script.js
+++ sitestatic/js/script.js
@@ -66,7 +66,7 @@ function responseHandler(jqXHR, textStatus, errorThrown, alertType) {
     console.groupEnd();
   } else {
     alert = "alert-success";
-    content = "This action has been saved successfully.";
+    content = "This action is done successfully.";
     title = "Success";
   }
   halfmoon.initStickyAlert({
@@ -76,3 +76,37 @@ function responseHandler(jqXHR, textStatus, errorThrown, alertType) {
     fillType: "filled",
   });
 }
+
+/**
+ * Things to do once the DOM is loaded
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  // Getting the required elements
+  var searchRepoInputElem = document.getElementById("filter-menu");
+
+  // Adding filtering to the link section ids in the sidebar
+  var linkSectionIDsList = new List("sidebar", {
+    valueNames: ["name", "alt-name"],
+  });
+
+  // Handle keydown events (overridden)
+  halfmoon.keydownHandler = function (event) {
+    // Shortcuts are triggered only if no input, textarea, or select has focus,
+    // And if the control key or command key is not pressed down
+    if (
+      !(
+        document.querySelector("input:focus") ||
+        document.querySelector("textarea:focus") ||
+        document.querySelector("select:focus")
+      )
+    ) {
+      if (!(event.ctrlKey || event.metaKey)) {
+        // Focus on the filter link element when [shift] + [F] keys are  pressed, but only if sidebar is not hidden (since the filter box is in the sidebar)
+        if (event.shiftKey && event.which == 70) {
+          searchRepoInputElem.focus();
+          event.preventDefault();
+        }
+      }
+    }
+  };
+});
