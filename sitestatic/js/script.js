c86b88d5e396146e333ce7e05279e5c307b4c0f7
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-08-12 16:02:10 +0100

    changed format and small fixes

--- sitestatic/js/script.js
+++ sitestatic/js/script.js
@@ -2,32 +2,37 @@
  * Things to do once the DOM is loaded
  */
 document.addEventListener("DOMContentLoaded", function () {
-  // Getting the required elements
-  var searchRepoInputElem = document.getElementById("filter-menu");
+    // Getting the required elements
+    let searchRepoInputElem = document.getElementById("filter-menu");
 
-  // Adding filtering to the link section ids in the sidebar
-  var linkSectionIDsList = new List("sidebar", {
-    valueNames: ["name", "alt-name"],
-  });
+    // Adding filtering to the link section ids in the sidebar
+    let linkSectionIDsList = new List("sidebar", {
+        valueNames: ["name", "alt-name"],
+    });
 
-  // Handle keydown events (overridden)
-  halfmoon.keydownHandler = function (event) {
-    // Shortcuts are triggered only if no input, textarea, or select has focus,
-    // And if the control key or command key is not pressed down
-    if (
-      !(
-        document.querySelector("input:focus") ||
-        document.querySelector("textarea:focus") ||
-        document.querySelector("select:focus")
-      )
-    ) {
-      if (!(event.ctrlKey || event.metaKey)) {
-        // Focus on the filter link element when [shift] + [F] keys are  pressed, but only if sidebar is not hidden (since the filter box is in the sidebar)
-        if (event.shiftKey && event.which == 70) {
-          searchRepoInputElem.focus();
-          event.preventDefault();
+    // Handle keydown events (overridden)
+    halfmoon.keydownHandler = function (event) {
+        /*
+        Shortcuts are triggered only if no input, textarea, or select has focus,
+        And if the control key or command key is not pressed down
+        */
+        if (
+            !(
+                document.querySelector("input:focus") ||
+                document.querySelector("textarea:focus") ||
+                document.querySelector("select:focus")
+            )
+        ) {
+            if (!(event.ctrlKey || event.metaKey)) {
+                /*
+                Focus on the filter link element when [shift] + [F] keys are  pressed, but only if
+                sidebar is not hidden (since the filter box is in the sidebar)
+                */
+                if (event.which === 70 && event.shiftKey) {
+                    searchRepoInputElem.focus();
+                    event.preventDefault();
+                }
+            }
         }
-      }
-    }
-  };
+    };
 });
