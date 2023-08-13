111edaef163764b0a185003b575ed7505c174381
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-08-13 18:52:44 +0100

    error handling

--- accounts/views/update_profile.py
+++ accounts/views/update_profile.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
@@ -12,14 +12,30 @@ Users = get_user_model()
 def update_profile_view(request):
     """Update a user profile"""
     user = Users.objects.get(pk=request.user.id)
-    profile = UserProfile.objects.get_or_create(user=user)
+    profile = None
+    try:
+        profile = UserProfile.objects.get(user=user)
+    except UserProfile.DoesNotExist:
+        pass
 
     if request.method == "POST":
         user_form = UserForm(request.POST, instance=user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if profile_form.is_valid() and user_form.is_valid():
-            user_form.save()
-            profile_form.save()
+            update_user = user_form.save(commit=False)
+            update_user.first_name = request.POST.get("first_name")
+            update_user.last_name = request.POST.get("last_name")
+            update_user.save()
+            if profile:
+                profile_form.save()
+            else:
+                new_profile = profile_form.save(commit=False)
+                new_profile.user = user
+                new_profile.title = request.POST.get("title", "")
+                new_profile.phone = request.POST.get("phone", "")
+                new_profile.about = request.POST.get("about", "")
+                new_profile.avatar = request.FILES.get("avatar", "")
+                new_profile.save()
             return redirect("accounts:user_profile")
 
     return redirect("accounts:user_profile")
