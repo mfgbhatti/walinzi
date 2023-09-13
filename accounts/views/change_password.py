2ee34e14c36f0e50d8a1e7f81c4235b06d09ebb3
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-09-13 13:33:00 +0100

    wip redirecting after changing password

--- accounts/views/change_password.py
+++ accounts/views/change_password.py
@@ -11,9 +11,17 @@ Users = get_user_model()
 def change_password_view(request):
     user_email = request.user.email
     user = Users.objects.get(email=user_email)
+    context = {
+        "redirect_url": "accounts:user_logout",
+        "redirct_wait": 5000
+    }
     if request.method == "POST":
         old_password = request.POST.get("password1")
+        confirmed_password = request.POST.get("password2")
         new_password = request.POST.get("password3")
+
+        if confirmed_password == new_password:
+            messages.error(request, "Passwords are not matching")
 
         user_authenicated = authenticate(email=user_email, password=old_password)
 
@@ -25,7 +33,8 @@ def change_password_view(request):
         else:
             user.set_password(new_password)
             user.save()
-            messages.success("Your password is changed")
+            messages.success(request, "Your password is changed")
             return redirect("accounts:user_logout")
+            # return render(request, "redirect.html", context)
 
     return redirect("accounts:user_profile")
