f61a74ac32b62c65f89e5d95fd040b3b61281b37
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-09-17 13:48:27 +0100

    client model etc

--- backend/client/models.py
+++ backend/client/models.py
@@ -1,3 +1,23 @@
+"""models for cleints"""
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """model for client"""
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    name = models.CharField(max_length=70, blank=False, null=False, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        db_table = "client"
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+        ordering = ("name",)
