298552b69602ac47d1052b2cf70727c461f50902
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-09-20 01:02:09 +0100

    small update to django config

--- config/settings/base.py
+++ config/settings/base.py
@@ -107,11 +107,17 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    # "DEFAULT_AUTHENTICATION_CLASSES": [
+    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
+    # ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    # "DEFAULT_PERMISSION_CLASSES": [
+    #     "rest_framework.permissions.IsAuthenticated",
+    # ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
