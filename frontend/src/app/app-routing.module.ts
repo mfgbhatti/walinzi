e0818484ebd706da80ec706f93ee2ed803765559
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-09-20 01:01:49 +0100

    updated app and customer deleted

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'client', loadChildren: () => import('./client/feature/client.module').then( module => module.ClientModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-// my components
-import { MainNavComponent } from './page_layout/main-nav/main-nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// my app
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppNavComponent } from './home/ui/nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    AppNavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule
   ],
