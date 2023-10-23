e8509d4d2edb061a0dc275464fc83bacc2225eee
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-10-23 10:08:19 +0100

    debugging auth guard

--- frontend/src/app/auth/gaurds/auth.guard.ts
+++ frontend/src/app/auth/gaurds/auth.guard.ts
@@ -1,28 +1,28 @@
 import { inject } from '@angular/core';
 import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
-import { AuthService } from 'src/app/auth/data-access/auth.services';
 import { of, switchMap } from 'rxjs';
+
+import { AuthService } from '../data-access/auth.services';
+
 
 export const AuthGuard: CanActivateFn | CanActivateChildFn = (route, state) => {
   const router: Router = inject(Router);
 
   // Check the authentication status
-  return inject(AuthService)
-    .check()
-    .pipe(
-      switchMap((authenticated) => {
-        // If the user is not authenticated...
-        if (!authenticated) {
-          // Redirect to the sign-in page with a redirectUrl param
-          const redirectURL =
-            state.url === '/sign-out' ? '' : `redirectURL=${state.url}`;
-          const urlTree = router.parseUrl(`sign-in?${redirectURL}`);
-
-          return of(urlTree);
-        }
-
-        // Allow the access
-        return of(true);
-      })
-    );
+  return inject(AuthService).check().pipe(
+    switchMap((authenticated) => {
+      console.log(authenticated)
+      // If the user is not authenticated...
+      if (!authenticated) {
+        // Redirect to the sign-in page with a redirectUrl param
+        const redirectURL = state.url === '/sign-out' ? '' : `redirectURL=${state.url}`;
+        const urlTree = router.parseUrl(`sign-in?${redirectURL}`);
+        console.log(authenticated)
+        return of(urlTree);
+      }
+      console.log(authenticated)
+      // Allow the access
+      return of(true);
+    }),
+  );
 };
