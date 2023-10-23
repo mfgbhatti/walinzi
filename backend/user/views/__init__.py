a016d62b57f7e23b17b1a777b047879ec183a81c
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-10-23 10:08:51 +0100

    clean up

--- backend/user/views/__init__.py
+++ backend/user/views/__init__.py
@@ -1,4 +1,3 @@
 from .user import UserViewSet
-from .auth import LoginView
 from .activation import user_activation_view
 from .authenicate import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView
