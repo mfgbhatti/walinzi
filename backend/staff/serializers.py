d09b8db876c7eb5700ffa41a30ea0c146406a0ed
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-10-26 09:15:27 +0100

    working staff shift details

--- backend/staff/serializers.py
+++ backend/staff/serializers.py
@@ -1,11 +1,34 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from backend.staff.models import Staff
+from backend.shift.models import Shift, Timesheet
 
 
-class StaffSerializer(serializers.ModelSerializer):
+class StaffSerializer(ModelSerializer):
     class Meta:
         model = Staff
         fields = "__all__"
 
     """Assuming that customer is  passed in the request"""
+
+
+class StaffShiftSerializer(ModelSerializer):
+    class Meta:
+        model = Shift
+        fields = "__all__"
+
+
+class StaffTimesheetSerializer(ModelSerializer):
+    # only single shift is supported
+    shift = StaffShiftSerializer()
+
+    class Meta:
+        model = Timesheet
+        fields = (
+            "id",
+            "staff",
+            "shift_date",
+            "duration",
+            "notes",
+            "shift",
+        )
--- backend/staff/views.py
+++ backend/staff/views.py
@@ -2,8 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from backend.staff.models import Staff
-from backend.staff.serializers import StaffSerializer
-from backend.shift.serializers import TimesheetSerializer
+from backend.staff.serializers import StaffSerializer, StaffTimesheetSerializer
 from backend.shift.models import Timesheet
 
 
@@ -18,11 +17,15 @@ class StaffViewSet(ModelViewSet):
         )  # Assuming user's client is associated with the user object
         return Staff.objects.filter(client=user_client)
 
+
 class StaffTimesheetViewSet(ModelViewSet):
     queryset = Timesheet.objects.all()
-    serializer_class = TimesheetSerializer
+    serializer_class = StaffTimesheetSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         staff_id = self.request.query_params.get("staff_id")
         return Timesheet.objects.filter(staff=staff_id)
+
+
+"""TODO: add support for date selection for staff shifts"""
