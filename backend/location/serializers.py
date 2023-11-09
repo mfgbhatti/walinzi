78f6c86c515fe58975edab0d09ff4453cdd0c9f1
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-11-09 10:03:22 +0000

    managed fields

--- backend/location/serializers.py
+++ backend/location/serializers.py
@@ -1,11 +1,31 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, CharField
 
 from backend.location.models import Location
 
 
-class LocationSerializer(serializers.ModelSerializer):
+class LocationSerializer(ModelSerializer):
+    mobile = CharField(required=False, allow_null=True, allow_blank=True)
+    reference = CharField(required=False, allow_blank=True, allow_null=True)
+    customer_name = CharField(source="customer.name")
+
     class Meta:
         model = Location
-        fields = "__all__"
+        fields = (
+            "id",
+            "name",
+            "mobile",
+            "email",
+            "land_line",
+            "address",
+            "post_code",
+            "city",
+            "is_active",
+            "reference",
+            "created_at",
+            "updated_at",
+            "charge_rate",
+            "customer",
+            "customer_name",
+        )
 
     """Assuming that customer is  passed in the request"""
