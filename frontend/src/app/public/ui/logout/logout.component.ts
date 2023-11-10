eb313ed577debbd5e6f97d253fcf8ea89348c703
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-11-10 15:18:57 +0000

    now navigating from auth service

--- frontend/src/app/public/ui/logout/logout.component.ts
+++ frontend/src/app/public/ui/logout/logout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { finalize, Subject, takeUntil, takeWhile, tap, timer } from 'rxjs';
-import { Router, RouterLink } from '@angular/router';
+import { finalize, takeUntil, takeWhile, tap, timer } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { Destroy } from '@shared/utils/destroy';
 import { AuthService } from 'src/app/auth/data-access/auth.services';
@@ -9,34 +9,34 @@ import { AuthService } from 'src/app/auth/data-access/auth.services';
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss'],
-  providers: [Destroy]
+  providers: [Destroy],
 })
 export class LogoutComponent implements OnInit {
-  countdown: number = 10;
+  countdown: number = 5;
   countdownMapping: any = {
     '=1': '# second',
-    'other': '# seconds',
+    other: '# seconds',
   };
 
-  private _authService = inject(AuthService)
-  private _destroy = inject(Destroy)
-  private _router = inject(Router)
+  private _authService = inject(AuthService);
+  private _destroy = inject(Destroy);
 
   ngOnInit(): void {
     // Sign out
-    this._authService.signOut().pipe(takeUntil(this._destroy)).subscribe();
 
     // Redirect after the countdown
     timer(1000, 1000)
       .pipe(
         finalize(() => {
-          this._router.navigate(['sign-in']);
+          this._authService
+            .signOut()
+            .pipe(takeUntil(this._destroy))
+            .subscribe();
         }),
         takeWhile(() => this.countdown > 0),
         takeUntil(this._destroy),
-        tap(() => this.countdown--),
+        tap(() => this.countdown--)
       )
       .subscribe();
   }
-
 }
