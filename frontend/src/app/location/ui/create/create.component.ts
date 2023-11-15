2ce05ad32bab84079a568f097d19196860d78671
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-11-15 16:42:48 +0000

    fixed form

--- frontend/src/app/location/ui/create/create.component.ts
+++ frontend/src/app/location/ui/create/create.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Observable } from 'rxjs/internal/Observable';
 
@@ -10,42 +14,46 @@ import { CustomerService } from 'src/app/customer/data-access/customer.services'
 @Component({
   selector: 'create-location',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateLocationComponent {
   form!: UntypedFormGroup;
   customers$!: Observable<Customer[] | null>;
   status_list: Array<any> = [
-    { status: true, label: "Active" },
-    { status: false, label: "Inactive" }
-  ]
+    { status: true, label: 'Active' },
+    { status: false, label: 'Inactive' },
+  ];
 
   constructor(
     private readonly formbuilder: UntypedFormBuilder,
     private readonly _customerService: CustomerService,
     public readonly dialogRef: MatDialogRef<CreateLocationComponent>,
     @Inject(MAT_DIALOG_DATA) private readonly data: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.customers$ = this._customerService.getAll();
+    this.customers$ = this._customerService.customers$;
     this.setForm();
   }
 
   setForm() {
     this.form = this.formbuilder.group({
       name: [this.data.name, [Validators.required, Validators.minLength(5)]],
-      land_line: [this.data.land_line, [Validators.required, Validators.pattern('[- +()0-9]+')]],
-      mobile: [this.data.mobile],
-      address: [this.data.address, [Validators.required]],
-      city: [this.data.city, [Validators.required]],
+      customer_id: [this.data.customer, [Validators.required]],
+      address: [this.data.address, Validators.required],
       post_code: [this.data.post_code, [Validators.required]],
-      email: [this.data.email, [Validators.required, Validators.email]],
-      is_active: [this.data.is_active, [Validators.required]],
-      charge_rate: [this.data.charge_rate, [Validators.required, Validators.min(0)]],
+      city: [this.data.city, [Validators.required]],
+      charge_rate: [
+        this.data.charge_rate,
+        [Validators.required, Validators.min(0)],
+      ],
+      is_active: [this.data.is_active, Validators.required],
+
+      land_line: [this.data.land_line, Validators.pattern('[- +()0-9]+')],
+      mobile: [this.data.mobile],
+      email: [this.data.email, Validators.email],
       reference: [this.data.reference],
-      customer: [this.data.customer, [Validators.required]],
-    })
+    });
   }
 
   close() {
