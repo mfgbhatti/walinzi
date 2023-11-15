593656bb42f9f2a63c3e21932d352355fa3ffc97
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-11-15 09:42:23 +0000

    wip location

--- backend/location/serializers.py
+++ backend/location/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
+from backend.customer.models import Customer
 from backend.location.models import Location
 
 
 class LocationSerializer(ModelSerializer):
     mobile = CharField(required=False, allow_null=True, allow_blank=True)
     reference = CharField(required=False, allow_blank=True, allow_null=True)
-    customer_name = CharField(source="customer.name")
+    customer_name = CharField(
+        source="customer.name", required=False, allow_blank=True, allow_null=True
+    )
 
     class Meta:
         model = Location
@@ -28,4 +31,52 @@ class LocationSerializer(ModelSerializer):
             "customer_name",
         )
 
-    """Assuming that customer is  passed in the request"""
+    def create(self, validated_data):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            client = request.user.client
+        else:
+            client = None
+        customer_data = validated_data.pop("customer")
+        customer_name = validated_data.pop("customer_name")
+        if customer_data is not None:
+            customer = Customer.objects.get(id=customer_data)
+            if customer is not None and customer.client == client:
+                new_location = Location.objects.create(
+                    customer=customer, **validated_data
+                )
+                return new_location
+            
+        return None
+
+    # def update(self, instance, validated_data):
+    #     user = None
+    #     request = self.context.get("request")
+    #     if request and hasattr(request, "user"):
+    #         client = request.user.client
+    #     else:
+    #         client = None
+    #     customer_data = validated_data.pop("customer")
+    #     customer_name = validated_data.pop("customer_name")
+    #
+    #     instance.name = validated_data.get("name", instance.name)
+    #     instance.email = validated_data.get("email", instance.email)
+    #     instance.mobile = validated_data.get("mobile", instance.mobile)
+    #     instance.land_line = validated_data.get("land_line", instance.land_line)
+    #     instance.address = validated_data.get("address", instance.address)
+    #     instance.city = validated_data.get("city", instance.city)
+    #     instance.post_code = validated_data.get("post_code", instance.post_code)
+    #     instance.is_active = validated_data.get("is_active", instance.is_active)
+    #     instance.reference = validated_data.get("reference", instance.reference)
+    #     instance.charge_rate = validated_data.get("charge_rate", instance.charge_rate)
+    #     if customer_data is not None:
+    #         customer = Customer.objects.get(id=customer_data)
+    #         if customer is not None and customer.client == client:
+    #             instance.customer = customer
+    #             instance.save()
+    #     else:
+    #         return None
+    #     instance.save()
+    #
+    #     return instance
