064790f82cd15b543c76c2870d6392804585f97d
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-12-05 12:46:33 +0000

    added horizontal layout

---
+++ frontend/src/app/layout/horizontal/horizontal.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@Component({
+  standalone: true,
+  selector: 'horizontal',
+  templateUrl: './horizontal.component.html',
+  styleUrls: ['./horizontal.component.scss'],
+  imports: [
+    NgIf,
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
+})
+export class HorizontalComponent {
+
+  constructor() {
+  }
+}
