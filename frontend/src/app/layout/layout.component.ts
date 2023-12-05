11d116fd8185c9135d7d0fa1430d70988fd05b3b
Farhan Ghani <13787498+mfgbhatti@users.noreply.github.com>
2023-12-05 12:47:32 +0000

    updated layout component

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,35 +1,69 @@
-import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatMenuModule } from '@angular/material/menu';
 
-import { AuthService } from 'src/app/auth/data-access/auth.services';
+import { HorizontalComponent } from './horizontal/horizontal.component';
+import { EmptyLayoutComponent } from './empty/empty.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-  ],
-  providers: [AuthService],
+  imports: [CommonModule, HorizontalComponent, EmptyLayoutComponent],
 })
-export class LayoutComponent {
-  _authService = inject(AuthService);
-  // change it to false
-  isLogedIn$!: Observable<boolean>;
+export class LayoutComponent implements OnInit {
+  layout!: string;
 
-  constructor() {
-    this.isLogedIn$ = this._authService.check();
+  private _activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.updateLayout();
+  }
+
+  updateLayout(): void {
+    let route = this._activatedRoute;
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    // // 1. Set the layout from the config
+    // this.layout = this.config.layout;
+
+    // 2. Get the query parameter from the current route and
+    // set the layout and save the layout to the config
+    const layoutFromQueryParam = route.snapshot.queryParamMap.get('layout');
+    // console.log(layoutFromQueryParam);
+    if (layoutFromQueryParam) {
+      this.layout = layoutFromQueryParam;
+    }
+    // 3. Iterate through the paths and change the layout as we find
+    // a config for it.
+    //
+    // The reason we do this is that there might be empty grouping
+    // paths or componentless routes along the path. Because of that,
+    // we cannot just assume that the layout configuration will be
+    // in the last path's config or in the first path's config.
+    //
+    // So, we get all the paths that matched starting from root all
+    // the way to the current activated route, walk through them one
+    // by one and change the layout as we find the layout config. This
+    // way, layout configuration can live anywhere within the path and
+    // we won't miss it.
+    //
+    // Also, this will allow overriding the layout in any time so we
+    // can have different layouts for different routes.
+    const paths = route.pathFromRoot;
+    paths.forEach((path) => {
+      // Check if there is a 'layout' data
+      if (
+        path.routeConfig &&
+        path.routeConfig.data &&
+        path.routeConfig.data['layout']
+      ) {
+        // Set the layout
+        this.layout = path.routeConfig.data['layout'];
+        // console.log(this.layout);
+      }
+    });
   }
 }
